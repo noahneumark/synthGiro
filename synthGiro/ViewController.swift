8dc5efbcd90e2ff6520482d7d2d37eda3d8a9566
Noah Neumark <noahneumark@gmail.com>
2017-04-27 12:01:31 -0700

    Initial Commit

---
+++ synthGiro/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  synthGiro
+//
+//  Created by Noah Neumark on 4/27/17.
+//  Copyright © 2017 Noah Neumark. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
